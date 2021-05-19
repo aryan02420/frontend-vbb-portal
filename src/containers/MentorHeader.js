7e660015ab231583aeb34ee88b595b461481a742
Mary Baker <meb601@gmail.com>
2021-05-19 17:27:06 -0400

    Change underscores to camel case for user name fields

--- src/containers/MentorHeader.js
+++ src/containers/MentorHeader.js
@@ -1,17 +1,17 @@
-import { Button, Dropdown, Menu, PageHeader } from 'antd';
-import React, { useEffect } from 'react';
+import React from 'react';
+import { PageHeader } from 'antd';
 import { connect } from 'react-redux';
 import * as actions from '../redux/actions';
-import { CaretDownOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { MenuUnfoldOutlined } from '@ant-design/icons';
 
 /**
  *  Top navigation bar for mentor
  * @param {Object} props.user Mentor profile information
- * @param {()=>void} props.onClose Function to toggle the left drawer for site nav
+ * @param {()=>void} props.onDrawerClose Function to toggle the left drawer for site nav
  * @returns React element
  */
 
-const MentorHeader = ({ user, logOut, onDrawerClose }) => {
+const MentorHeader = ({ user, onDrawerClose }) => {
 
   return (
     <>
@@ -23,33 +23,6 @@ const MentorHeader = ({ user, logOut, onDrawerClose }) => {
         avatar={{
           src: 'https://picsum.photos/100',
         }}
-        extra={[
-          <Button key="3">Scheduler</Button>,
-          <Button key="2">Calendar</Button>,
-          <Dropdown
-            key="more"
-            overlay={
-              <Menu>
-                <Menu.Item>
-                  <a onClick={logOut}>Logout</a>
-                </Menu.Item>
-              </Menu>
-            }
-          >
-            <Button key="1" type="primary">
-              <span
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                {user.first_name}{' '}
-                <CaretDownOutlined style={{ marginLeft: '5px' }} />
-              </span>
-            </Button>
-          </Dropdown>,
-        ]}
       />
     </>
   );
