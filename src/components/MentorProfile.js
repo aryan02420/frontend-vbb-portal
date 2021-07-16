fe3ebf6ae3ff183df9ac56aff7962da07177a658
aryan02420 <aryan02420@gmail.com>
2021-07-16 19:32:17 +0530

    make fields editable

--- src/components/MentorProfile.js
+++ src/components/MentorProfile.js
@@ -1,60 +1,134 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../redux/actions'
 import { Form, Input, Button, Row, Col } from 'antd'
-import {
-  UserOutlined,
-  LockOutlined,
-  EyeInvisibleOutlined,
-  EyeTwoTone,
-} from '@ant-design/icons'
 
-function MentorProfile({ user }) {
-  // email: "testMentor@test.com"
-  // external_id: "db6e2abe-91b8-4180-bf9c-d518766669ef"
-  // first_name: "MentorFirstName"
-  // id: "db6e2abe-91b8-4180-bf9c-d518766669ef"
-  // last_name: "MentorLastName"
-  // user_type: "MENTOR"
+const MentorProfile = ({ user, setUser, updateUserInfo }) => {
+  const [editInput, setEditInput] = useState(false)
 
-  console.log(user)
+  const onChange = (key) => (evt) => {
+    console.log({ value: evt.target.value, key })
+    setUser({ ...user, [key]: evt.target.value })
+  }
+
+  const onSubmit = () => {
+    updateUserInfo()
+  }
 
   return (
     <div>
       <div>
         <hr />
         <br />
-        <h2 style={{ color: '#4C4C49' }}>My Info</h2>
+        <div style={{ display: 'flex' }}>
+          <h2 style={{ color: '#4C4C49' }}>My Info</h2>
+          <Button
+            type={editInput ? 'primary' : 'default'}
+            htmlType="button"
+            onClick={(e) => {
+              setEditInput(!editInput)
+              if (editInput) {
+                // onSubmit
+              }
+              e.preventDefault()
+            }}
+            style={{ marginLeft: '1rem' }}
+          >
+            {editInput ? 'Save' : 'Edit'}
+          </Button>
+        </div>
         <br />
         <h3 style={{ color: '#FF914D' }}>Account-Related</h3>
         <p>
           <Row justify="left" align="left">
             <Col span={12} style={{}}>
-              <Form.Item label="Name">
+              <Form.Item label="First Name">
                 <Input
                   type="text"
                   style={{ color: 'black' }}
-                  value={`${user.firstName} ${user.lastName}`}
-                  disabled="true"
+                  onChange={onChange('firstName')}
+                  value={`${user.firstName}`}
+                  disabled={!editInput}
+                />
+              </Form.Item>
+              <Form.Item label="Last Name">
+                <Input
+                  type="text"
+                  style={{ color: 'black' }}
+                  onChange={onChange('lastName')}
+                  value={`${user.lastName}`}
+                  disabled={!editInput}
                 />
               </Form.Item>
               <Form.Item label="Email">
                 <Input
-                  type="text"
+                  type="email"
                   value={user.email}
+                  onChange={onChange('email')}
                   style={{ color: 'black' }}
-                  disabled="true"
+                  disabled={!editInput}
                 />
               </Form.Item>
               <Form.Item label="Phone">
                 <Input
-                  type="text"
+                  type="tel"
                   value={user.phone}
+                  onChange={onChange('phone')}
                   style={{ color: 'black' }}
-                  disabled="true"
+                  disabled={!editInput}
                 />
               </Form.Item>
-              <br />
+              <Form.Item label="Date of Birth">
+                <Input
+                  type="date"
+                  value={user.dateOfBirth}
+                  onChange={onChange('dateOfBirth')}
+                  style={{ color: 'black' }}
+                  disabled={!editInput}
+                />
+              </Form.Item>
+              <Form.Item label="City">
+                <Input
+                  type="text"
+                  value={user.city}
+                  onChange={onChange('city')}
+                  style={{ color: 'black' }}
+                  disabled={!editInput}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </p>
+        <br />
+        <h3 style={{ color: '#549BEA' }}>Mentor-Related</h3>
+        <p>
+          <Row justify="left" align="left">
+            <Col span={12} style={{}}>
+              <Form.Item label="My Languages">
+                <Input
+                  type="text"
+                  value={user.languages}
+                  onChange={onChange('languages')}
+                  style={{ color: 'black' }}
+                  disabled={!editInput}
+                />
+              </Form.Item>
+              <Form.Item label="My Time Zone">
+                <Input
+                  type="text"
+                  value={user.timezone}
+                  onChange={onChange('timezone')}
+                  style={{ color: 'black' }}
+                  disabled={!editInput}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </p>
+        <br />
+        <Row>
+          <Col>
+            <Form>
               <Form.Item label="Current Password">
                 <Input.Password />
               </Form.Item>
@@ -73,33 +147,9 @@ function MentorProfile({ user }) {
               >
                 Change Password
               </Button>
-            </Col>
-          </Row>
-        </p>
-        <br />
-        <h3 style={{ color: '#549BEA' }}>Mentor-Related</h3>
-        <p>
-          <Row justify="left" align="left">
-            <Col span={12} style={{}}>
-              <Form.Item label="My Languages">
-                <Input
-                  type="text"
-                  value="English"
-                  style={{ color: 'black' }}
-                  disabled="true"
-                />
-              </Form.Item>
-              <Form.Item label="My Time Zone">
-                <Input
-                  type="text"
-                  value="ET"
-                  style={{ color: 'black' }}
-                  disabled="true"
-                />
-              </Form.Item>
-            </Col>
-          </Row>
-        </p>
+            </Form>
+          </Col>
+        </Row>
       </div>
     </div>
   )
